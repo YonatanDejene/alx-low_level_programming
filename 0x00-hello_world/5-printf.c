d1320c833082e3084be12152b1e4db2c972d8ed4
YonatanDejene <yonatan.dejene@gmail.com>
2023-03-17 11:24:58 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,15 @@
-#include<stud.io>
+#include <stud.io>
 
-/*
+/**
  * main prints "output"
  *
  *
  * Return always 0
+ *
 */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
