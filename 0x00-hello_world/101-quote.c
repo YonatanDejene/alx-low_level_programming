60c3468be0cf3c5a2acee98b3986e704a1cf10c2
YonatanDejene <yonatan.dejene@gmail.com>
2023-03-17 14:13:25 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - prints "a string (give)"
- * compiling process and ..
- * Return: Always 1. 
+ * main - prints "a string (give)",
+ * compiling process.
+ * Return: Always 1.
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (1);
 }
