cbf4b4c91d2a31f0408f28f370917eb1d031dba5
YonatanDejene <yonatan.dejene@gmail.com>
2023-03-17 14:05:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 
 /**
  * main - prints "a string (give)"
- *
  * Return: Always 
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
