8ddc9075b49554afaa4176006f54131b90dd74a1
YonatanDejene <yonatan.dejene@gmail.com>
2023-03-19 12:36:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the alphabet in lower case 
+ * compilation..
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a';  c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
