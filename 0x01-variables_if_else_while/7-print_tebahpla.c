752119342c60b1e594744298a8a7ff74764e9aa3
YonatanDejene <yonatan.dejene@gmail.com>
2023-03-19 13:41:59 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in revers
+ * compiles..
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+}
