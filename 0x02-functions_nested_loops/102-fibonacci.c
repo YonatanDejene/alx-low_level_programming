0eb458710c072b032ffcfe569eea72f05552d50c
YonatanDejene <yonatan.dejene@gmail.com>
2023-03-21 14:25:43 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,19 +6,23 @@
  *
  */
 
-int main() {
-    int n, first = 1, second = 2, next;
-
-    for (n = 0; n < 50; n++) {
-        if (n <= 1) {
-            next = n + 1;
-        } else {
-            next = first + second;
-            first = second;
-            second = next;
-        }
-        printf("%d ", next);
-    }
-
-    return 0;
+int main(void)
+{
+	int n, first = 1, second = 2, next;
+	
+	for (n = 0; n < 50; n++)
+	{
+	if (n <= 1)
+	{
+	next = n + 1;
+	}
+	else
+	{
+	next = first + second;
+	first = second;
+	second = next;
+	}
+	printf("%d, ", next);
+	}
+	return (0);
 }
