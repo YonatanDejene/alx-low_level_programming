7dcf30bc3a6f39aa09648a3ef1b89d25b5208696
YonatanDejene <yonatan.dejene@gmail.com>
2023-03-21 10:26:49 -0700

    ./6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of a number
+ * Return: Always Success
+ */
+
+int _abs(int n)
+{
+	int ab = 0;
+ 
+	if (n < 0)
+	{
+	ab = -1 * n;
+	return (ab);
+	}
+	if (n > 0)
+	{
+	ab = n;
+	return (ab);
+	}
+	return (ab);
+}
