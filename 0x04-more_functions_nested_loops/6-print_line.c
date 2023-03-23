6768bd7792c225aa840bf12b7760cbcf2e51943e
YonatanDejene <yonatan.dejene@gmail.com>
2023-03-23 13:35:06 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * Return: Nothing to retrun
+ */
+
+void print_line(int n)
+{
+	int i;
+	for (i = 1; i <= n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+}
