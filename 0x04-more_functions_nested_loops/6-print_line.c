a74ad08389f54a95f0686a86539100bc7b34ad3b
YonatanDejene <yonatan.dejene@gmail.com>
2023-03-23 14:26:07 -0700

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_line - prints a line
- * @n: the value passed
+ * print_line - Draws a line according to parameter
+ * @n: The number of lines to draw
  * Return: empty
  */
 
 void print_line(int n)
 {
 	int i;
-	
+
 	if (n <= 0)
 	{
 	_putchar('\n');
@@ -17,7 +17,7 @@ void print_line(int n)
 	else
 	{
 	for (i = 1; i <= n; i++)
-	{
+	}
 	_putchar('_');
 	}
 	_putchar('\n');
