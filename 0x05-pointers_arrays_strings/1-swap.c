9b0076a5e89ee1fa79dc704488d8a1709c3ea7be
YonatanDejene <yonatan.dejene@gmail.com>
2023-03-28 09:03:16 -0700

    commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int - swaps the value of two integers
- * @*a: pointer to first integer
- * @*b: pointer to second integer
+ * @a: pointer to first integer
+ * @b: pointer to second integer
  *
  * Return: nothing
  */
