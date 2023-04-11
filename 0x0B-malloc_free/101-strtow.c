cca44704e3a9f801a5aca386689fe4f3690c9d8f
YonatanDejene <yonatan.dejene@gmail.com>
2023-04-10 21:09:41 -0700

    ls

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,7 +16,7 @@ int word_len(char *str)
 {
 	int index = 0, len = 0;
 
-	while(*(str + index) && *(str + index) != ' ' )
+	while (*(str + index) && *(str + index) != ' ')
 	{
 		len++;
 		index++;
@@ -26,7 +26,7 @@ int word_len(char *str)
 }
 
 /**
- * count_words - Counts the number of words contained 
+ * count_words - Counts the number of words contained
  * @str: The string to be searched.
  *
  * Return: The number of words contained
@@ -38,7 +38,6 @@ int count_words(char *str)
 
 	for (index = 0; *(str + index); index++)
 		len++;
-	
 	for (index = 0; index < len; index++)
 	{
 		if (*(str + index) != ' ')
