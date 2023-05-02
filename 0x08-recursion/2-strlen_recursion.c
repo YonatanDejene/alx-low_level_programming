819a5effb319ec027527eb454dd551eef5a3d7b8
YonatanDejene <33670253+YonatanDejene@users.noreply.github.com>
2023-05-02 14:48:24 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,11 +9,11 @@
 
 int _strlen_recursion(char*s)
 {
-  if (*s == '\0')
-  {
-    return (0);
-  }
-  
-  s++;
-  return (_strlen_recursion(s) + 1);
+	if (*s == '\0')
+{
+	return (0);
+}
+
+	s++;
+	return (_strlen_recursion(s) + 1);
 }
