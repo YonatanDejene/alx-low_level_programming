36f6fe8ab942aa52d80ccecf53cfa0a084fddad8
YonatanDejene <yonatan.dejene@gmail.com>
2023-05-05 01:20:21 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - Convert decimal int to binary.
+ * @n: Decimal number.
+ */
+
+void print_binary(unsigned long int n)
+{
+	int j, c = 0;
+	unsigned long int crnt;
+
+	for (j = 63; j >= 0; j--)
+	{
+		crnt = n >> j;
+
+		if (crnt & 1)
+		{
+			-putchar('1');
+			c++;
+		}
+		else if (c)
+			_putchar('0');
+	}
+	if (!c)
+		_putchar('0');
+}
