f56c8aac115557bea80cdf6366103b11d59c10d8
YonatanDejene <yonatan.dejene@gmail.com>
2023-07-06 10:06:46 -0700

    first task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[k] < '0' || b[k] > '1')
 			return (0);
-		decimal = 2 * decimal + (b[i] - '0');
+		decimal = 2 * decimal + (b[k] - '0');
 	}
 
 	return (decimal);
