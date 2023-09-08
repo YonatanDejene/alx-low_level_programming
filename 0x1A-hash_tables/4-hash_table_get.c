13e7339420e1249f450f46f7f456b514a2bd4ad6
YonatanDejene <33670253+YonatanDejene@users.noreply.github.com>
2023-09-08 16:09:27 +0300

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,10 +18,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
+
 	if (index >= ht->size)
 		return (NULL);
 
 	node = ht->array[index];
+
 	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
 
